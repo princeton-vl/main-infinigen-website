f8abc88f88d899e3d13646c72aec7ce26f457b13
Lahav Lipson <llipson@princeton.edu>
2023-05-01 02:59:04 -0400

    misc

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -129,7 +129,7 @@ const config = {
           },
           { to: "/blog", label: "Blog", position: "left" },
           {
-            href: "https://github.com/facebook/docusaurus",
+            href: "https://github.com/princeton-vl",
             label: "GitHub",
             position: "right",
           },
