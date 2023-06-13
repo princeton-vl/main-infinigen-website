9d9e380f55fa16da43d42970f9c120783b52b271
Lahav Lipson <llipson@princeton.edu>
2023-06-12 23:09:54 -0400

    Update features and capabilities

--- src/components/KeyFeaturesAndCapabilities.tsx
+++ src/components/KeyFeaturesAndCapabilities.tsx
@@ -2,30 +2,25 @@ import React from "react";
 import clsx from "clsx";
 import styles from "../pages/index.module.css";
 
-const Capability = () => {
+const Capability = (props) => {
   return (
-    <div className={clsx(styles.capability_outer, "padding-left--lg")}>
-      <div className={"row"}>
-        <div className="col col--5">
-          <img
-            src={"placeholder.png"}
-            className={styles.capability_image}
-          ></img>
+    <div className="item shadow--lw">
+      <div className={clsx(styles.capability_outer)}>
+        <div className={"row"}>
+          <div className="col col--1" />
+          <div className="col col--10">{props.children}</div>
         </div>
-      </div>
-      <div className={"row"}>
-        <div className="col col--12 margin-top--md">
-          <h1>Lorem Ipsum</h1>
+        <div className={"row"}>
+          <div className="col col--1" />
+          <div className="col col--10 margin-top--md">
+            <h1>{props.header}</h1>
+          </div>
         </div>
-      </div>
-      <div className={"row"}>
-        <div className="col col--12">
-          <p>
-            tellus orci ac auctor augue mauris augue neque gravida in fermentum
-            et sollicitudin ac orci phasellus egestas tellus rutrum tellus
-            pellentesque eu tincidunt tortor aliquam nulla facilisi cras
-            fermentum odio
-          </p>
+        <div className={"row"}>
+          <div className="col col--1" />
+          <div className="col col--10">
+            <p>{props.text}</p>
+          </div>
         </div>
       </div>
     </div>
@@ -39,18 +34,136 @@ export default function KeyFeaturesAndCapabilities(): JSX.Element {
         <h1 className="hero__title">Key Features & Capabilities</h1>
         <div className={"row"}>
           <div className="col col--6">
-            <Capability />
+            <Capability
+              header="Procedural"
+              text="All assets, including the terrain, fauna and flora, are generated entirely from code and a random seed.
+              Infinigen does not rely on any third-party asset libraries.
+              Users can modify the generation code for any asset to change the distribution of resulting meshes and materials.
+              The code for each asset contains a set of random parameters which can be set manually or seeded randomly."
+            >
+              <img
+                src={"img/procedural.png"}
+                className={styles.capability_image}
+              ></img>
+            </Capability>
           </div>
           <div className="col col--6">
-            <Capability />
+            <Capability
+              header="Diverse"
+              text="Infinigen contains dozens of pre-written templates of different types of terrain, fauna and flora. Each template is a Python class defining a distribution of assets with randomizable parameters.
+              Users can extend the existing system by writing their own asset generators."
+            >
+              <img
+                src={"img/diverse.png"}
+                className={styles.capability_image}
+              ></img>
+            </Capability>
           </div>
         </div>
         <div className={clsx("row")}>
           <div className="col col--6">
-            <Capability />
+            <Capability
+              header="Real Geometry"
+              text="Infinigen treats researchers as first-class citizens;
+              all high-frequency detail is modeled using high-resolution geometry, so our annotations are 100% accurate.
+              Infinigen doesn't use bump/normal-maps, full-transparency, or any other techniques which add the illusion of detail that isn't really there."
+            >
+              <div className={clsx("row", styles.real_vs_fake_imgs)}>
+                <div className="col col--6">
+                  <div>
+                    <img src={"img/real_geo.png"}></img>
+                  </div>
+                  <div style={{ textAlign: "center" }}>
+                    Art, Games & Movies use
+                    <br />
+                    <span
+                      style={{
+                        borderRadius: "2px",
+                        color: "red",
+                        padding: "0.2rem",
+                        fontWeight: "bold",
+                      }}
+                    >
+                      Fake Geometry
+                    </span>
+                  </div>
+                </div>
+                <div className="col col--6">
+                  <div>
+                    <img
+                      src={"img/real_geo.png"}
+                      className={styles.capability_image}
+                    ></img>
+                  </div>
+                  <div style={{ textAlign: "center" }}>
+                    Infinigen uses
+                    <br />
+                    <span
+                      style={{
+                        borderRadius: "2px",
+                        color: "green",
+                        padding: "0.2rem",
+                        fontWeight: "bold",
+                      }}
+                    >
+                      Real Geometry
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </Capability>
           </div>
+
           <div className="col col--6">
-            <Capability />
+            <Capability
+              header="High-Quality Annotations"
+              text="Infinigen generates dense annotations for a variety of computer vision tasks, including optical flow, 3D scene flow, depth, surface normals, panoptic segmentation, occlusion boundaries, and various render-passes (albedo, specular, etc.)."
+            >
+              <div className={clsx("row", styles.real_vs_fake_imgs)}>
+                <div className="col col--6">
+                  <div>
+                    <img
+                      style={{ border: "1px solid #1a1e13" }}
+                      src={"img/depth_example.png"}
+                    ></img>
+                  </div>
+                </div>
+                <div className="col col--6">
+                  <div>
+                    <img
+                      style={{ border: "1px solid #1a1e13" }}
+                      src={"img/surface_normal_example.png"}
+                      className={styles.capability_image}
+                    ></img>
+                  </div>
+                </div>
+              </div>
+              <div
+                className={clsx(
+                  "row",
+                  "margin-top--md",
+                  styles.real_vs_fake_imgs
+                )}
+              >
+                <div className="col col--6">
+                  <div>
+                    <img
+                      style={{ border: "1px solid #1a1e13" }}
+                      src={"img/segmentation_example.png"}
+                    ></img>
+                  </div>
+                </div>
+                <div className="col col--6">
+                  <div>
+                    <img
+                      style={{ border: "1px solid #1a1e13" }}
+                      src={"img/flow_example.png"}
+                      className={styles.capability_image}
+                    ></img>
+                  </div>
+                </div>
+              </div>
+            </Capability>
           </div>
         </div>
       </div>
