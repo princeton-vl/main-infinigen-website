16cfdf6fdd62a8ea3b555644a290b187a0a32602
lahav-token <llipson@princeton.edu>
2024-06-15 17:47:55 -0400

    Indent.

--- src/components/PaperInfo.tsx
+++ src/components/PaperInfo.tsx
@@ -18,11 +18,11 @@ export default function PaperInfo(): JSX.Element {
   const bibtex_indoors =
     "\
 @inproceedings{FILL THIS OUT,\n\
-title={Infinigen Indoors: Photorealistic Indoor Scenes using Procedural Generation},\n\
-author={FILL THIS OUT},\n\
-booktitle={Proceedings of the IEEE/CVF Conference on Computer Vision and Pattern Recognition},\n\
-pages={FILL THIS OUT},\n\
-year={2024}\n\
+  title={Infinigen Indoors: Photorealistic Indoor Scenes using Procedural Generation},\n\
+  author={FILL THIS OUT},\n\
+  booktitle={Proceedings of the IEEE/CVF Conference on Computer Vision and Pattern Recognition},\n\
+  pages={FILL THIS OUT},\n\
+  year={2024}\n\
 }";
   return (
     <section className={clsx("margin-top--lg")}>
