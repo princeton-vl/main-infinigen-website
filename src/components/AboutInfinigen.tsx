75e0e0d52174862339eb786f38948855a6c47136
lahav-token <llipson@princeton.edu>
2024-06-15 17:11:01 -0400

    Update main fig.

--- src/components/AboutInfinigen.tsx
+++ src/components/AboutInfinigen.tsx
@@ -28,7 +28,7 @@ export default function AboutInfinigen(): JSX.Element {
     <section className={clsx("margin-top--lg")}>
       <div className={styles.stay_within_navbar + " container"}>
         <div className="row">
-          <div className={clsx("col col--7", styles.about_infinigen_left)}>
+          <div className={clsx("col col--6", styles.about_infinigen_left)}>
             <img
               src={"img/random_sample.jpeg"}
               className={clsx(
@@ -37,27 +37,39 @@ export default function AboutInfinigen(): JSX.Element {
               )}
             ></img>
           </div>
-          <div className={clsx("col col--5")}>
-            <h1>
-              About <span className={styles.infinigen_name}>Infinigen</span>
-            </h1>
-            <p>
-              Infinigen is a procedural generator of 3D scenes, developed by{" "}
-              <a href="https://pvl.cs.princeton.edu/">
-                Princeton Vision & Learning Lab
-              </a>
-              . Infinigen is optimized for computer vision research and
-              generates diverse high-quality 3D training data. Infinigen is
-              based on <a href="https://blender.org">Blender</a> and is free and
-              open-source (BSD 3-Clause License). Infinigen is being actively
-              developed to expand its capabilities and coverage. Everyone is
-              welcome to <a href="./docs-contributing">contribute</a>.
-            </p>
-            <TwitterTimelineEmbed
+          <div className={clsx("col col--6", styles.about_infinigen_left)}>
+            <img
+              src={"img/random_sample_indoors.jpeg"}
+              className={clsx(
+                styles.about_infinigen_img
+                // styles.vertical_center
+              )}
+            ></img>
+          </div>
+          <div className="row margin-top--md">
+            <div className={clsx("col col--12")}>
+              <h1>
+                About <span className={styles.infinigen_name}>Infinigen</span>
+              </h1>
+              <p>
+                Infinigen is a procedural generator of 3D scenes, developed by{" "}
+                <a href="https://pvl.cs.princeton.edu/">
+                  Princeton Vision & Learning Lab
+                </a>
+                . Infinigen is optimized for computer vision research and
+                generates diverse high-quality 3D training data. Infinigen is
+                based on <a href="https://blender.org">Blender</a> and is free
+                and open-source (BSD 3-Clause License). Infinigen is being
+                actively developed to expand its capabilities and coverage.
+                Everyone is welcome to{" "}
+                <a href="./docs-contributing">contribute</a>.
+              </p>
+              {/* <TwitterTimelineEmbed
               sourceType="profile"
               screenName="PrincetonVL"
               options={{ height: 180 }}
-            />
+            /> */}
+            </div>
           </div>
         </div>
       </div>
